1be1d7e7d87a35daee89068bb6e29613952ce44a
awanish singh <awanishsingh2991@gmail.com>
2026-08-16 18:11:56 +0530

    tflint updated again_tflint or

--- prod/child_module/Storage_account/variable.tf
+++ prod/child_module/Storage_account/variable.tf
@@ -1,1 +1,3 @@
-variable "stgacc" {}
+variable "stgacc" {
+    
+}
--- prod/parent module/variable.tf
+++ prod/parent module/variable.tf
@@ -1,4 +1,36 @@
-variable "rgs" {}
-variable "vnet" {}
-variable "stgacc" {}
-variable "subid" {}
+variable "rgs" {
+  type = map(object({
+    name     = string
+    location = string
+  }))
+}
+
+variable "vnet" {
+  type = map(object({
+    rgn      = string
+    name     = string
+    location = string
+    as       = list(string)
+  }))
+}
+
+variable "stgacc" {
+  type = map(object({
+    name                  = string
+    rgn                   = string
+    location              = string
+    at                    = string
+    art                   = string
+    contname              = string
+    container_access_type = string
+  }))
+}
+
+variable "subid" {
+  type = map(object({
+    name         = string
+    rgn          = string
+    virtnet_name = string
+    ap           = list(string)
+  }))
+}
